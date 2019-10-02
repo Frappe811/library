8479f9504ea0087f47ac53b1515db1b94f51c7bb
minhanhd <minh.duong@deliveree.com>
2019-10-02 10:46:04 +0700

    Add booking controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
-class ApplicationController < ActionController::Base
+class BookingController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def new
+  	redirect_to 'new'
+  end
+
+  def create
+  	Booking.create(booking_id: params[:id])
+  end
+
+  private
+
+  def params
+  	params.require(:booking_id)
+  end
 end
